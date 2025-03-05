e95d12219a5c71d1b10f01fa4aad6223e2b5e853
Monique Duarte <moniquead95@gmail.com>
2025-03-05 17:47:55 -0300

    pages

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import Header from 'Components/Header';
+import Menu from 'Components/Menu';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import Sobre from 'pages/Sobre';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default function AppRouter() {
+  return (
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<Header />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
+          </ Route>
+        </Routes>
+      </Router>
+    </main>
+  );
+}

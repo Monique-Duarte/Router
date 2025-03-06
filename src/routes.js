a89b4bd09c5037fe5dcdfd8b1395c3fbffe098ca
Monique Duarte <moniquead95@gmail.com>
2025-03-06 09:26:28 -0300

    footer

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
+import Footer from 'Components/Footer';
 import Header from 'Components/Header';
 import Menu from 'Components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
 import Sobre from 'pages/Sobre';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <Menu />
         <Routes>
@@ -17,7 +19,9 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
           </ Route>
+          <Route path='*' element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
